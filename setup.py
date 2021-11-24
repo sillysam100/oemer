25e08f9abc245827440a217a6f4ac4b685e13ab6
BreezeWhite <miyashia2010@tuta.io>
2021-11-24 20:04:16 +0800

    Add metadata to the package

--- setup.py
+++ setup.py
@@ -6,23 +6,24 @@ with open("README.md") as red:
 
 setuptools.setup(
     name='oemer',
-    version='0.1.0-rc6',
+    version='0.1.0rc6',
     author='BreezeWhite',
     author_email='miyasihta2010@tuta.io',
     description='End-to-end Optical Music Recognition (OMR) system.',
     long_description=ldest,
     long_description_content_type='text/markdown',
+    license='License :: OSI Approved :: MIT License',
     license_files=('LICENSE',),
     url='https://github.com/BreezeWhite/oemer',
     packages=setuptools.find_packages(),
     package_data={
-    '': [
-            'sklearn_models/*.model',
-            'checkpoints/unet_big/metadata.pkl',
-            'checkpoints/unet_big/arch.json',
-            'checkpoints/seg_net/metadata.pkl',
-            'checkpoints/seg_net/arch.json',
-        ]
+        '': [
+                'sklearn_models/*.model',
+                'checkpoints/unet_big/metadata.pkl',
+                'checkpoints/unet_big/arch.json',
+                'checkpoints/seg_net/metadata.pkl',
+                'checkpoints/seg_net/arch.json',
+            ]
     },
     install_requires=[
         'onnxruntime-gpu',
@@ -36,5 +37,21 @@ setuptools.setup(
     extras_require={
         'full': ['tensorflow-gpu', 'tf2onnx']
     },
-    entry_points={'console_scripts': ['oemer = oemer.ete:main']}
+    entry_points={'console_scripts': ['oemer = oemer.ete:main']},
+    keywords=['OMR', 'optical-music-recognition', 'AI', 'machine-learning', 'image-processing'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Education',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Image Processing',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Version Control :: Git'
+    ]
 )
